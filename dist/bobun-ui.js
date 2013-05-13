63767c4fbc48d720f66addc6bf451a94ba78432a
Greg Bergé <neoziro@gmail.com>
2013-05-13 23:43:47 +0200

    bump to version 0.3.3

--- dist/bobun-ui.js
+++ dist/bobun-ui.js
@@ -1,4 +1,4 @@
-/*! bobun-ui - v0.3.1 - https://github.com/neoziro/bobun-ui */
+/*! bobun-ui - v0.3.3 - https://github.com/neoziro/bobun-ui */
 (function () {
   'use strict';
 
@@ -275,6 +275,58 @@
       return this;
     }
   });
+}).call(this);
+(function () {
+  'use strict';
+
+  var Bobun = this.Bobun;
+  Bobun.UI = Bobun.UI || {};
+
+  Bobun.UI.Input = Bobun.View.extend({
+
+    tagName: 'input',
+
+    attributes: {
+      type: 'text'
+    },
+
+    events: {
+      'click'   : 'domEventTriggerProxy',
+      'change'  : 'domEventTriggerProxy',
+      'input'   : 'domEventTriggerProxy',
+      'keydown' : 'domEventTriggerProxy',
+      'keyup'   : 'domEventTriggerProxy',
+      'keypress': 'domEventTriggerProxy'
+    },
+
+    options: {
+      value: null,
+      disabled: false
+    },
+
+    initialize: function () {
+      // events
+      this.on('change:disabled', this.updateDisabled);
+      this.on('change:value', this.updateValue);
+    },
+
+    render: function () {
+      return this
+      .updateDisabled()
+      .updateValue();
+    },
+
+    updateDisabled: function () {
+      this.$el.prop('disabled', this.get('disabled'));
+      return this;
+    },
+
+    updateValue: function () {
+      this.$el.val(this.get('value'));
+      return this;
+    }
+  });
+
 }).call(this);
 (function () {
   'use strict';
