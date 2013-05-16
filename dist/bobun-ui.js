cfa40ffb54c007d8469980b4f60df74fc6e57e71
Greg Bergé <neoziro@gmail.com>
2013-05-16 17:02:13 +0200

    bump to version 0.4.1

--- dist/bobun-ui.js
+++ dist/bobun-ui.js
@@ -336,6 +336,35 @@
 (function () {
   'use strict';
 
+  var Bobun = this.Bobun;
+  Bobun.UI = Bobun.UI || {};
+
+  Bobun.UI.Label = Bobun.View.extend({
+
+    tagName: 'label',
+
+    events: {
+      'click'   : 'domEventTriggerProxy'
+    },
+
+    options: {
+      label: null
+    },
+
+    initialize: function () {
+      this.on('change:label', this.render);
+    },
+
+    render: function () {
+      this.$el.html(this.get('label'));
+      return this;
+    }
+  });
+
+}).call(this);
+(function () {
+  'use strict';
+
   var Backbone = this.Backbone,
   Bobun = this.Bobun;
   Bobun.UI = Bobun.UI || {};
@@ -473,6 +502,87 @@
       this.views.each(this.append, this);
       return this;
     }
+  });
+
+}).call(this);
+(function () {
+  'use strict';
+
+  var Bobun = this.Bobun,
+  _ = this._;
+  Bobun.UI = Bobun.UI || {};
+
+  Bobun.UI.Select = Bobun.View.extend({
+
+    tagName: 'select',
+
+    events: {
+      'change'  : 'domEventTriggerProxy'
+    },
+
+    options: {
+      value: null,
+      choices: [],
+      disabled: false
+    },
+
+    initialize: function () {
+      this.on('change:disabled', this.renderDisabled);
+      this.on('change:choices change:value', this.checkValue);
+      this.on('change:choices', this.renderChoices);
+      this.on('change:value', this.renderValue);
+
+      this.on('$change', this.updateValue);
+
+      this.checkValue();
+    },
+
+    checkValue: function () {
+      var value = _
+      .chain(this.get('choices'))
+      .keys().find(function (value) {
+        return '' + value === '' + this.get('value');
+      }, this)
+      .value();
+
+      this.set('value', _.isUndefined(value) ? _.chain(this.get('choices')).keys().first().value() : value);
+    },
+
+    updateValue: function () {
+      this.set('value', this.$el.val());
+    },
+
+    render: function () {
+      return this
+      .renderDisabled()
+      .renderChoices()
+      .renderValue();
+    },
+
+    renderDisabled: function () {
+      this.$el.prop('disabled', this.get('disabled'));
+      return this;
+    },
+
+    renderChoices: function () {
+      this.$el.empty();
+      _.each(this.get('choices'), this.renderChoice, this);
+      return this;
+    },
+
+    renderChoice: function (label, value) {
+      var $option = $('<option>')
+      .val(value)
+      .html(label);
+
+      return this.$el.append($option);
+    },
+
+    renderValue: function () {
+      this.$el.val(this.get('value'));
+      return this;
+    }
+
   });
 
 }).call(this);
